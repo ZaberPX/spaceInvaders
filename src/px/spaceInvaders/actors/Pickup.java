0fa5a114ea2e4c02ef7657a361ff0bb1ca1442e7
Michael Stopa <mstopa42@gmail.com>
2013-01-25 01:18:42 +1030

    Added Pickup and Powerup classes

---
+++ src/px/spaceInvaders/actors/Pickup.java
@@ -0,0 +1,20 @@
+package px.spaceInvaders.actors;
+
+import javax.media.opengl.GLAutoDrawable;
+
+import org.lwjgl.util.vector.Vector2f;
+
+import px.spaceInvaders.graphics.Sprite;
+import px.spaceInvaders.graphics.SpriteMaster;
+
+/**TODO Class Description and all Methods
+ * @author Michael Stopa */
+public class Pickup extends Sprite {
+
+    public Pickup(GLAutoDrawable drawable, SpriteMaster master, String texture,
+            Vector2f location, Vector2f hitSize, Vector2f drawSize, float depth) {
+        super(drawable, master, texture, location, hitSize, drawSize, depth);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
